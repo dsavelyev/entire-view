8266d70bbdfc4724f71b5f59cc2e68f4a304b852
Dmitry Savelyev <swedmitrysav@gmail.com>
2026-05-15 19:19:16 +0200

    read checkpoint id

--- src/main/kotlin/EntireCheckpointView.kt
+++ src/main/kotlin/EntireCheckpointView.kt
@@ -2,9 +2,16 @@ package org.example
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.vcs.log.VcsLogDataKeys
 
 class EntireCheckpointView : AnAction() {
-    override fun actionPerformed(p0: AnActionEvent) {
-        TODO("Not yet implemented")
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val sel = e.getData(VcsLogDataKeys.VCS_LOG_COMMIT_SELECTION) ?: return
+        val commit = sel.commits.firstOrNull() ?: return
+
+        val checkpointId = getCheckpointId(project, commit.root, commit.hash.asString()) ?: return
     }
 }
